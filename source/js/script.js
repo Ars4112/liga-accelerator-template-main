1fcd966eb01c270e28b528d3beaa005a38ad8f40
Ars4112 <Ars4112@gmail.com>
2022-11-10 23:09:02 +0300

    добавляет PP

--- source/js/script.js
+++ source/js/script.js
@@ -54,21 +54,58 @@ const addScroll = () => {
   const mainPageButton = document.querySelector('.main-page__button');
   const form = document.querySelector('.form');
 
-  const isScroll = ()=> {
+  const isScroll = () => {
     form.scrollIntoView({
       block: 'start',
       behavior: 'smooth',
     });
   };
-  mainPageButton.addEventListener('keydown', (evt)=> {
+  mainPageButton.addEventListener('keydown', (evt) => {
     if (evt.key === 'Enter') {
       isScroll();
     }
   });
 
-
   mainPageButton.addEventListener('click', isScroll);
+};
+
+const addMask = () => {
+  document.addEventListener('DOMContentLoaded', function () {
+    let eventCalllback = function (e) {
+      let el = e.target;
+      let clearVal = el.dataset.phoneClear;
+      let pattern = el.dataset.phonePattern;
+      let matrixDef = '+7(___) ___-__-__';
+      let matrix = pattern ? pattern : matrixDef;
+      let i = 0;
+      let def = matrix.replace(/\D/g, '');
+      let val = e.target.value.replace(/\D/g, '');
+      if (clearVal !== 'false' && e.type === 'blur') {
+        if (val.length < matrix.match(/([\_\d])/g).length) {
+          e.target.value = '';
+          return;
+        }
+      }
+      if (def.length >= val.length) {
+        val = def;
+      }
+      e.target.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length
+          ? val.charAt(i++)
+          : i >= val.length
+            ? ''
+            : a;
+      });
+    };
+    let phoneInputs = document.querySelectorAll('.form__input--tel');
+    for (let elem of phoneInputs) {
+      for (let ev of ['input', 'blur', 'focus']) {
+        elem.addEventListener(ev, eventCalllback);
+      }
+    }
+  });
 };
 
 addText();
 addScroll();
+addMask();
